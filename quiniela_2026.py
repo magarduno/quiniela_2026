5d9c0c4debef084834e928e92cfc8e1390f63bc9
magarduno <m_angel1106@hotmail.com>
2026-06-04 09:35:24 -0600

    Update quiniela_2026.py

--- quiniela_2026.py
+++ quiniela_2026.py
@@ -671,6 +671,7 @@ else:
         if st.button("🚪 Salir",use_container_width=True): st.session_state.user=None; st.rerun()
         st.divider()
         st.info("Los partidos se bloquean una hora antes de comenzar el encuentro.")
+        st.info("EFECTIVO EN DESPACHO 💵,\n Depósitos Banco Azteca 💳 \n Tarjeta: 4027665885774530 \n MIGUEL ANGEL GARDUÑO LOPEZ")
         
     # ══════════════════════════════════════════
     # USUARIO NORMAL
